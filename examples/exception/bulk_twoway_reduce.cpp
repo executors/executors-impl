1e05ece54e7745c2630ae7f9dfe7c4a1077112e8
Lee Howes <lwh@fb.com>
2018-03-14 08:59:11 -0700

    Two exception reduction examples.

--- examples/exception/bulk_twoway_reduce.cpp
+++ examples/exception/bulk_twoway_reduce.cpp
@@ -7,9 +7,46 @@ using std::experimental::executors_v1::future;
 
 int main()
 {
-  static_thread_pool pool{8};
+  static_thread_pool pool{1};
   auto ex = execution::require(pool.executor(), execution::never_blocking);
-  future<int> f = ex.bulk_twoway_execute([](int n, int&, int&){ std::cout << "part " << n << "\n"; }, 8, []{ return 42; }, []{ return 0; });
-  f.wait();
-  std::cout << "result is " << f.get() << "\n";
+  future<std::pair<int, std::exception_ptr>> f = ex.bulk_twoway_execute(
+    [](
+        int n,
+        // Can't make atomic exception_ptr
+        std::pair<int, std::exception_ptr>& result,
+        int&){
+      try {
+        if(n == 5) {
+          throw std::logic_error("Have a 5");
+        } else {
+          result.first += n;
+        }
+      } catch(...) {
+        if(!result.second) {
+          result.second = std::current_exception();
+        }
+      }
+    },
+    8,
+    []() -> std::pair<int, std::exception_ptr>{ return {0, nullptr}; },
+    []{ return 0; });
+
+  // We don't need this if we have a good expected type that future can
+  // construct from
+  auto f2 = f.then([](future<std::pair<int, std::exception_ptr>> result) -> int {
+      auto p = result.get();
+      if(p.second) {
+         std::rethrow_exception(p.second);
+      } else {
+        return p.first;
+      }
+    });
+
+  try {
+    auto p = f2.get();
+    std::cout << "result is " << p << "\n";
+  } catch(std::logic_error& l) {
+    std::cout << "Exception: " << l.what() << "\n";
+  }
+
 }
