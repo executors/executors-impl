f391b517903650daa5717e434828c7c68854f087
Jared Hoberock <jaredhoberock@gmail.com>
2019-02-05 16:15:27 -0600

    Test reduce with on(static_thread_pool.executor())

---
+++ examples/reduce/reduce_2.cpp
@@ -0,0 +1,116 @@
+#include "reduce.hpp"
+#include "par.hpp"
+#include "throwing_iterator.hpp"
+#include <experimental/thread_pool>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <functional>
+#include <numeric>
+
+int main()
+{
+  {
+    // test with par, vector
+
+    std::vector<int> vec(10);
+    std::iota(vec.begin(), vec.end(), 1);
+
+    std::experimental::static_thread_pool pool(4);
+
+    int result = reduce(par.on(pool.executor()), vec.begin(), vec.end(), 13, std::plus<>());
+
+    assert(std::accumulate(vec.begin(), vec.end(), 13) == result);
+  }
+
+  {
+    // test with par, list
+
+    std::list<int> lst(10);
+    std::iota(lst.begin(), lst.end(), 1);
+
+    std::experimental::static_thread_pool pool(4);
+
+    int result = reduce(par.on(pool.executor()), lst.begin(), lst.end(), 13, std::plus<>());
+
+    assert(std::accumulate(lst.begin(), lst.end(), 13) == result);
+  }
+
+  // XXX the following tests correctly call std::terminate upon 
+  //     exception in an element access function
+  //     they are disabled to allow the test program to complete normally
+
+  //{
+  //  // test with par, vector, throwing function
+
+  //  std::vector<int> vec(10);
+  //  std::iota(vec.begin(), vec.end(), 1);
+  
+  //  std::experimental::static_thread_pool pool(4);
+
+  //  reduce(par.on(pool.executor()), vec.begin(), vec.end(), 13, [](int x, int y)
+  //  {
+  //    throw 13;
+
+  //    return x + y;
+  //  });
+
+  //  // this line should never be reached
+  //  assert(false);
+  //}
+
+  //{
+  //  // test with par, list, throwing function
+
+  //  std::list<int> lst(10);
+  //  std::iota(lst.begin(), lst.end(), 1);
+  
+  //  std::experimental::static_thread_pool pool(4);
+
+  //  reduce(par.on(pool.executor()), lst.begin(), lst.end(), 13, [](int x, int y)
+  //  {
+  //    throw 13;
+
+  //    return x + y;
+  //  });
+
+  //  // this line should never be reached
+  //  assert(false);
+  //}
+
+  //{
+  //  // test with par, vector, throwing iterator
+
+  //  std::vector<int> vec(10);
+  //  std::iota(vec.begin(), vec.end(), 1);
+
+  //  std::experimental::static_thread_pool pool(4);
+
+  //  reduce(par.on(pool.executor()), make_throwing_iterator(vec.begin()), make_throwing_iterator(vec.end()), 13, std::plus<>());
+
+  //  // this line should never be reached
+  //  assert(false);
+  //}
+
+  //{
+  //  // test with par, list, throwing iterator
+
+  //  std::list<int> lst(10);
+  //  std::iota(lst.begin(), lst.end(), 1);
+  
+  //  std::experimental::static_thread_pool pool(4);
+
+  //  reduce(par.on(pool.executor()), make_throwing_iterator(lst.begin()), make_throwing_iterator(lst.end()), 13, std::plus<>());
+
+  //  // this line should never be reached
+  //  assert(false);
+  //}
+
+  std::cout << "OK" << std::endl;
+
+  return 0;
+}
+
+
